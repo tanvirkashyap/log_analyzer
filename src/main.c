9000ff9f673eebfdee825c0024c7e17449d5134e
tanvirkashyap <tanvi.gm@gmail.com>
2026-05-05 10:42:34 +0530

    Main function

--- src/main.c
+++ src/main.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-
-void analyze_logs(FILE *fp);
+#include "analyzer.h" // Added missing header[cite: 1]
 
 int main() {
     FILE *fp = fopen("data/sample.log", "r");
-
     if (!fp) {
-        printf("Error opening file\n");
+        perror("Error opening file"); // More descriptive error[cite: 10]
         return 1;
     }
-
     analyze_logs(fp);
-
     fclose(fp);
     return 0;
 }
