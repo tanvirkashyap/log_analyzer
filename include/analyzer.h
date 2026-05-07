473c444198af701af42b5b4b4319fce3683a3ae1
tanvirkashyap <tanvi.gm@gmail.com>
2026-05-07 09:37:10 +0530

    Analyzer header library

--- include/analyzer.h
+++ include/analyzer.h
@@ -3,12 +3,6 @@
 
 #include <stdio.h>
 
-// Core analysis function
 void analyze_logs(FILE *fp);
-
-// Optional: advanced detection APIs
-void detect_bruteforce();
-void detect_suspicious_users();
-void detect_traffic_spike();
 
 #endif
