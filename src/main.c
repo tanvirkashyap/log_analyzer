a3b6e0af03a0727a276484f736bb2e7686433156
tanvirkashyap <tanvi.gm@gmail.com>
2026-05-07 09:32:48 +0530

    Main function fix

--- src/main.c
+++ src/main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
-#include "analyzer.h" // Added missing header[cite: 1]
-# include "cli.h"
-int main() {
-    FILE *fp = fopen("data/sample.log", "r");
-    if (!fp) {
-        perror("Error opening file"); // More descriptive error[cite: 10]
-        return 1;
+#include "analyzer.h"
+#include "cli.h"
+
+int main(int argc, char *argv[]) {
+    if (argc >= 2) {
+        handle_cli(argc, argv);
+    } else {
+        FILE *fp = fopen("data/sample.log", "r");
+        if (!fp) {
+            perror("Error opening file");
+            return 1;
+        }
+        analyze_logs(fp);
+        fclose(fp);
     }
-    analyze_logs(fp);
-    fclose(fp);
     return 0;
 }
